8e96214b1119fd9969f084f7f8e70a1e1249a88d
Amritaash Verma <amritaash.verma@gmail.com>
2024-03-13 00:40:16 +0530

    wikipedia page link extractor works!

--- python automation/Project #2 web automation and scrapping/all links in wiki page.py
+++ python automation/Project #2 web automation and scrapping/all links in wiki page.py
@@ -3,33 +3,26 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import pandas
 
-website = "https://www.wikipedia.org/wiki/States_and_union_territories_of_India"
-path = "E:\Amritaash\pythonProject\python automation\chrome_driver\chromedriver"
+website = "https://en.wikipedia.org/wiki/Olympic_Games"
+path = r'\'C:\Users\amrit\Desktop\My programming folders\My_python_projects\python automation\chrome_driver'
 
 service = Service(executable_path=path)
-driver = webdriver.Chrome(service=service)
+driver = webdriver.Chrome()
 driver.get(website)
-hatnote_elements = driver.find_elements(by="xpath", value='//div[@class="hatnote navigation-not-searchable"]')
+hatnote_elements = driver.find_elements(by="xpath", value='//div[@class="mw-content-ltr mw-parser-output"]/p/a')
 links = []
 texts = []
 
 for element in hatnote_elements:
-    link = element.find_element(by="xpath", value='.//a').get_attribute("href")
-    text = element.find_element(by="xpath", value='.//a').text
+    link = element.get_attribute("href")
+    text = element.text
     links.append(link)
     texts.append(text)
 
-p_elements = driver.find_elements(by="xpath", value='//div[@class="vector-body ve-init-mw-desktopArticleTarget-targetContainer"and @id="bodyContent"]/div[@class="mw-body-content mw-content-ltr" and @id="mw-content-text"]/div[@class="mw-parser-output"]')
-
-for p_tag_link in p_elements:
-    p_link = p_tag_link.find_element(by="xpath", value='.//p//a').get_attribute("href")
-    p_text = p_tag_link.find_element(by="xpath", value='.//p//a').text
-    links.append(p_link)
-    texts.append(p_text)
-
 my_dict = {'Links': links, 'Text in links': texts}
 data_frame = pandas.DataFrame(my_dict)
-data_frame.to_csv('E:\Amritaash\pythonProject\python automation\Project #2 web automation and scrapping\states.csv')
-data = pandas.read_csv('E:\Amritaash\pythonProject\python automation\Project #2 web automation and scrapping\states.csv')
-data.to_html('E:\Amritaash\pythonProject\python automation\Project #2 web automation and scrapping\states_csv_html.html')
+print(data_frame)
+data_frame.to_csv('python automation\Project #2 web automation and scrapping\links.csv', index=False)
+data = pandas.read_csv("python automation\Project #2 web automation and scrapping\links.csv")
+data.to_html("python automation\Project #2 web automation and scrapping\wiki_links.html")
 
--- python automation/Project #2 web automation and scrapping/automating a website.py
+++ python automation/Project #2 web automation and scrapping/automating a website.py
@@ -7,21 +7,18 @@ import sys
 
 #making a py an exe file
 application_path = os.path.dirname(sys.executable)
-
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 website = "https://www.thesun.co.uk/sport/football/"
-path = "E:\Amritaash\pythonProject\python automation\chrome_driver\chromedriver"
+path = r"\"C:\Users\amrit\Desktop\My programming folders\My_python_projects\python automation\chrome_driver\chromedriver.exe"
 
 #headless mode
 options = Options()
 options.headless = True
-
 service = Service(executable_path=path)
-driver = webdriver.Chrome(service=service, options=options)
+driver = webdriver.Chrome(options=options)
 driver.get(website)
 containers = driver.find_elements(by="xpath", value='//div[@class="teaser__copy-container"]')
-
 titles = []
 sub_titles = []
 links = []
@@ -37,10 +34,11 @@ for container in containers:
 my_dict = {'titles': titles, 'subtitles': sub_titles, 'links': links}
 
 df_headline = pandas.DataFrame(my_dict)
-df_headline.to_csv('E:\Amritaash\pythonProject\python automation\Project #2 web automation and scrapping\headline.csv')
+# df_headline.to_csv(r"\"C:\sers\amrit\Desktop\My programming-folders\My_python_projects\python automation\Project #2 web automation and scrapping\headline.csv")
 
 driver.quit()
-pandas.options.display.max_columns = None
-pandas.options.display.max_rows = None
-data = pandas.read_csv('E:\Amritaash\pythonProject\python automation\Project #2 web automation and scrapping\headline.csv')
-data.to_html("E:\Amritaash\pythonProject\python automation\Project #2 web automation and scrapping\csv_html.html")
+print(df_headline)
+# pandas.options.display.max_columns = None
+# pandas.options.display.max_rows = None
+# data = pandas.read_csv(r'\'C:\Users\amrit\Desktop\My programming folders\My_python_projects\python automation\Project #2 web automation and scrapping\headline.csv')
+# data.to_html(r"\"C:\Users\amrit\Desktop\My programming folders\My_python_projects\python automation\Project #2 web automation and scrapping\csv_html.html")
