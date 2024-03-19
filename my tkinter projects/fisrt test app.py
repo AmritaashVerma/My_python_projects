12973df063d1299154607fad630b297b629ea4dd
Amritaash Verma <amritaash.verma@gmail.com>
2024-03-19 20:08:49 +0530

    updation

--- my tkinter projects/fisrt test app.py
+++ my tkinter projects/fisrt test app.py
@@ -8,7 +8,7 @@ class App():
         self.root.title("A very simple test app") #the title bar of the app
         self.mainframe = tk.Frame(self.root, background="white") #over here we define the parent frame of the app in self and this is the frame in which all our UI and widgets will come 
         self.mainframe.pack(fill="both", expand=True)#this says the parent frame to span over the entire app
-        self.text = ttk.Label(self.mainframe, text="Hello world!", background="blue", font=("Harlow Solid Italic", 35))#adding text to the parent frame i.e. self.mainframe
+        self.text = ttk.Label(self.mainframe, text="Hello world!", background="black", font=("Harlow Solid Italic", 35))#adding text to the parent frame i.e. self.mainframe
         self.text.grid(row=0, column=0)#defining the location of the text
         self.set_text_field = ttk.Entry(self.mainframe)#this gives a text field to give our input
         self.set_text_field.grid(row= 2, column=0, sticky='NWES')#(sticky='NWES' EXPANDS THE TEXT FILED to the the full row width of previous element)) #this gives and input text field grid function puts the widget on the mainframe
