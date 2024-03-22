4db1faeccd57c49366ee994603e985a34f03ded7
Amritaash Verma <amritaash.verma@gmail.com>
2024-03-23 02:43:51 +0530

    pata nahi ji konsa nasha karta hai

--- python automation/Project #2 web automation and scrapping/all links in wiki page.py
+++ python automation/Project #2 web automation and scrapping/all links in wiki page.py
@@ -1,12 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 import pandas
 
-website = "https://en.wikipedia.org/wiki/Quantum_computing"
-path = r'\'C:\Users\amrit\Desktop\My programming folders\My_python_projects\python automation\chrome_driver'
+website = "https://en.wikipedia.org/wiki/Stock_market"
 
-service = Service(executable_path=path)
 driver = webdriver.Chrome()
 driver.get(website)
 hatnote_elements = driver.find_elements(by="xpath", value='//div[@class="mw-content-ltr mw-parser-output"]/p/a')
@@ -22,7 +18,7 @@ for element in hatnote_elements:
 my_dict = {'Links': links, 'Text in links': texts}
 data_frame = pandas.DataFrame(my_dict)
 print(data_frame)
-data_frame.to_csv('python automation\Project #2 web automation and scrapping\links.csv', index=False)
-data = pandas.read_csv("python automation\Project #2 web automation and scrapping\links.csv")
-data.to_html("python automation\Project #2 web automation and scrapping\wiki_links.html")
+data_frame.to_csv('links.csv', index=False)
+data = pandas.read_csv("links.csv")
+data.to_html("wiki_links.html")
 
