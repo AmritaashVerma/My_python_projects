42b06b129f54a02022fd8b2dcc7eba4d957eb865
Amritaash Verma <amritaash.verma@gmail.com>
2025-02-05 23:37:03 +0530

    this is a majorly python repo

--- python automation/Project #1 table extraction/extracting tables from PDFs.py
+++
@@ -1,7 +0,0 @@
-import camelot
-
-table = camelot.read_pdf(r'E:\Amritaash\pythonProject\python automation\Project #1 table extraction\foo.pdf', pages='1')
-
-print(table)
-
-#failed
--- python automation/Project #1 table extraction/random tables.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd 
-
-tables = pd.read_html('https://international.collegeboard.org/students/sat/acceptance-india')
-
-print(tables)
